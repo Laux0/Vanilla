b505c7ae4b49db5c1abc8a6d0f18f66f2752543e
Laux0 <laura.rahmati@gmail.com>
2025-08-10 23:02:53 +0200

    Added function to update heading with city input

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-alert("hello world");
+function searchAction(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let weatherCity = document.querySelector("#weather-city");
+  weatherCity.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-city");
+let searchButton = document.querySelector("#search-button");
+
+searchFormElement.addEventListener("submit", searchAction);
