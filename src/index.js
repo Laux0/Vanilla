ec019d2f4182c80fcd6a4f46804104287b3497e9
Laux0 <laura.rahmati@gmail.com>
2025-08-11 11:34:42 +0200

    Added function to access weather data from API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+function updateCityTemperature() {
+  let currentDayTemperature = document.querySelector("#temperature-value");
+}
+
+function findCityTemperature(city) {
+  let keyApi = "83bco8b8afca3aft80c7a9a59f08542a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${keyApi}&units=metric`;
+  axios.get(apiUrl).then(updateCityTemperature);
+}
+
 function searchAction(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let weatherCity = document.querySelector("#weather-city");
   weatherCity.innerHTML = searchInput.value;
+  findCityTemperature(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-city");
