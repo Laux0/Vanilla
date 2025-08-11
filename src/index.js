01b432280b87a7a0239f7f58775cb581cd785956
Laux0 <laura.rahmati@gmail.com>
2025-08-11 11:44:15 +0200

    Added function to display current temperature from the API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-function updateCityTemperature() {
+function updateCityTemperature(response) {
   let currentDayTemperature = document.querySelector("#temperature-value");
+  let temperature = Math.round(response.data.temperature.current);
+  currentDayTemperature.innerHTML = `${temperature}`;
 }
 
 function findCityTemperature(city) {
   let keyApi = "83bco8b8afca3aft80c7a9a59f08542a";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${keyApi}&units=metric`;
   axios.get(apiUrl).then(updateCityTemperature);
+  console.log(apiUrl);
 }
 
 function searchAction(event) {
